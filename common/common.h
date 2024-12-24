7b413174df26adbfb7c92af509001e98e1f383c7
slymnyldrm <slymn.yildirim98@gmail.com>
2024-12-24 11:17:21 +0400

    added new messages for onboard_mission pipelines

--- common/common.h
+++ common/common.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_COMMON.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_COMMON_XML_HASH -3641707666068524671
+#define MAVLINK_COMMON_XML_HASH 9174965300328138177
 
 #ifdef __cplusplus
 extern "C" {
